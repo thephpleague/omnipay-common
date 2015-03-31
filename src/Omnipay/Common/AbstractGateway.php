928766775540de295fd973b470d8cc33eb02bf3d
Frederik Bosch <f.bosch@genkgo.nl>
2015-03-31 11:16:53 +0200

    Should fix #30

--- src/Omnipay/Common/AbstractGateway.php
+++ src/Omnipay/Common/AbstractGateway.php
@@ -296,6 +296,10 @@ abstract class AbstractGateway implements GatewayInterface
      */
     protected function getDefaultHttpRequest()
     {
+        HttpRequest::setFactory(function ($query, $request, $attributes, $cookies, $files, $server, $content) {
+            return new HttpRequest($query, $request, $attributes, $cookies, [], $server, $content);
+        });
+
         return HttpRequest::createFromGlobals();
     }
 }
