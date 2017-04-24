f9fb5e9aac12c10277e137238b723a92e7a7d3a7
Barry vd. Heuvel <barryvdh@gmail.com>
2017-04-24 22:34:29 +0200

    Simplify redirectresponse method

--- src/Omnipay/Common/Message/AbstractResponse.php
+++ src/Omnipay/Common/Message/AbstractResponse.php
@@ -205,6 +205,53 @@ abstract class AbstractResponse implements ResponseInterface
      */
     public function getRedirectResponse()
     {
+        $this->validateRedirect();
+
+        if ('GET' === $this->getRedirectMethod()) {
+            return HttpRedirectResponse::create($this->getRedirectUrl());
+        }
+
+        $hiddenFields = '';
+        foreach ($this->getRedirectData() as $key => $value) {
+            $hiddenFields .= sprintf(
+                '<input type="hidden" name="%1$s" value="%2$s" />',
+                htmlentities($key, ENT_QUOTES, 'UTF-8', false),
+                htmlentities($value, ENT_QUOTES, 'UTF-8', false)
+            )."\n";
+        }
+
+        $output = '<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Redirecting...</title>
+</head>
+<body onload="document.forms[0].submit();">
+    <form action="%1$s" method="post">
+        <p>Redirecting to payment page...</p>
+        <p>
+            %2$s
+            <input type="submit" value="Continue" />
+        </p>
+    </form>
+</body>
+</html>';
+        $output = sprintf(
+            $output,
+            htmlentities($this->getRedirectUrl(), ENT_QUOTES, 'UTF-8', false),
+            $hiddenFields
+        );
+
+        return HttpResponse::create($output);
+    }
+
+    /**
+     * Validate that the current Response is a valid redirect.
+     *
+     * @return void
+     */
+    protected function validateRedirect()
+    {
         if (!$this instanceof RedirectResponseInterface || !$this->isRedirect()) {
             throw new RuntimeException('This response does not support redirection.');
         }
@@ -213,43 +260,8 @@ abstract class AbstractResponse implements ResponseInterface
             throw new RuntimeException('The given redirectUrl cannot be empty.');
         }
 
-        if ('GET' === $this->getRedirectMethod()) {
-            return HttpRedirectResponse::create($this->getRedirectUrl());
-        } elseif ('POST' === $this->getRedirectMethod()) {
-            $hiddenFields = '';
-            foreach ($this->getRedirectData() as $key => $value) {
-                $hiddenFields .= sprintf(
-                    '<input type="hidden" name="%1$s" value="%2$s" />',
-                    htmlentities($key, ENT_QUOTES, 'UTF-8', false),
-                    htmlentities($value, ENT_QUOTES, 'UTF-8', false)
-                )."\n";
-            }
-
-            $output = '<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>Redirecting...</title>
-    </head>
-    <body onload="document.forms[0].submit();">
-        <form action="%1$s" method="post">
-            <p>Redirecting to payment page...</p>
-            <p>
-                %2$s
-                <input type="submit" value="Continue" />
-            </p>
-        </form>
-    </body>
-</html>';
-            $output = sprintf(
-                $output,
-                htmlentities($this->getRedirectUrl(), ENT_QUOTES, 'UTF-8', false),
-                $hiddenFields
-            );
-
-            return HttpResponse::create($output);
+        if (!in_array($this->getRedirectMethod(), ['GET', 'POST'])) {
+            throw new RuntimeException('Invalid redirect method "'.$this->getRedirectMethod().'".');
         }
-
-        throw new RuntimeException('Invalid redirect method "'.$this->getRedirectMethod().'".');
     }
 }
