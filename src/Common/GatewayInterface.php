1df3d09e5fd69b1ef4e4b33e719d38fec313bc32
zouyi <php-cpm@users.noreply.github.com>
2018-01-11 17:23:53 +0800

    fix SNIFF VIOLATIONS

--- src/Common/GatewayInterface.php
+++ src/Common/GatewayInterface.php
@@ -40,7 +40,6 @@ interface GatewayInterface
      * Get gateway display name
      *
      * This can be used by carts to get the display name for each gateway.
-     * 
      * @return string
      */
     public function getName();
@@ -50,7 +49,6 @@ interface GatewayInterface
      *
      * This name can be used with GatewayFactory as an alias of the gateway class,
      * to create new instances of this gateway.
-     * 
      * @return string
      */
     public function getShortName();
@@ -63,21 +61,18 @@ interface GatewayInterface
      *     'testMode' => false, // boolean variable
      *     'landingPage' => array('billing', 'login'), // enum variable, first item is default
      * );
-     * 
      * @return array
      */
     public function getDefaultParameters();
 
     /**
      * Initialize gateway with parameters
-     * 
      * @return $this
      */
     public function initialize(array $parameters = array());
 
     /**
      * Get all gateway parameters
-     *
      * @return array
      */
     public function getParameters();
